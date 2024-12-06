ccb1f6dafd3e65f58ebf525e13383cb583dc94cd
Egor Alexandrov <dibilarts@gmail.com>
2024-12-06 14:05:12 +0700

    Added Book model to db, reworked the views, added new model to admin panel

--- sitemmm/meh/models.py
+++ sitemmm/meh/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    year = models.CharField(max_length=7)
+    about = models.TextField()
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.username} - {self.title}'
--- sitemmm/meh/views.py
+++ sitemmm/meh/views.py
@@ -1,32 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-books_list = [
-    {
-        'author': 'Haruki Murakami',
-        'title': 'Hear the Wind Sing',
-        'year': '1979',
-        'about': 'First part of the Rat Thrilogy'
-    },
-    {
-        'author': 'Haruki Murakami',
-        'title': 'Pinball, 1973',
-        'year': '1980',
-        'about': 'Second part of the Rat Thrilogy'
-    },
-    {
-        'author': 'Haruki Murakami',
-        'title': 'A Wild Sheep Chase',
-        'year': '1982',
-        'about': 'Third part of the Rat Thrilogy'
-    },
-    {
-        'author': 'Haruki Murakami',
-        'title': 'Dance, Dance, Dance',
-        'year': '1994',
-        'about': 'Ending part of the Rat Thrilogy'
-    }
-]
+from .models import Book
 
 # Create your views here.
 def home(request):
@@ -34,6 +8,6 @@ def home(request):
 
 def books(request):
     context = {
-        'books': books_list
+        'books': Book.objects.all()
     }
     return render(request, 'meh/books.html', context)
