cff799747abf5b522f98f809fe4197bd92b61dcd
Egor Alexandrov <dibilarts@gmail.com>
2024-12-09 10:27:06 +0700

    added register form

--- sitemmm/meh/views.py
+++ sitemmm/meh/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
 
-# Create your views here.
 def home(request):
     return render(request, 'meh/home.html')
 
--- sitemmm/sitemmm/settings.py
+++ sitemmm/sitemmm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'meh.apps.MehConfig',
     'django.contrib.admin',
     'django.contrib.auth',
