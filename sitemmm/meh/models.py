9e01ceaee8aab088382d67d6d0121248166f548f
Egor Alexandrov <dibilarts@gmail.com>
2025-01-06 00:00:37 +0700

    Added pages for books of certain user and for certain author

--- sitemmm/meh/models.py
+++ sitemmm/meh/models.py
@@ -8,6 +8,12 @@ class Book(models.Model):
     year = models.CharField(max_length=7)
     about = models.TextField()
     username = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_url = models.CharField(max_length=100, blank=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        self.author_url = self.author.replace(" ", "_")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.username} - {self.title}'
+    
--- sitemmm/meh/views.py
+++ sitemmm/meh/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 def home(request):
     return render(request, 'meh/home.html')
+
+class BookListView(ListView):
+    model = Book
+    template_name = "meh/books.html"
+    context_object_name = 'books'
+    ordering = ['-id']
+
+class AuthorListView(ListView):
+    model = Book
+    template_name = "meh/author_detail.html"
+    ordering = ['-id']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        author_url = self.kwargs['pk']
+        context['books'] = Book.objects.filter(author_url=author_url)  # Фильтруем книги по author_url
+        return context
+
+class UserListView(ListView):
+    model = Book
+    template_name = "meh/user_books.html"
+    ordering = ['-id']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.kwargs['pk']
+        context['books'] = Book.objects.filter(username__username=username)  # Фильтруем книги по author_url
+        return context
 
 def books(request):
     context = {
